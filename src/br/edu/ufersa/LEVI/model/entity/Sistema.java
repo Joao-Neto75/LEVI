1e426e379c28c7f97f63bea603d9c763a40a5681
Joao-Neto75 <joaosilva.lgl@gmail.com>
2026-04-26 17:41:30 -0300

    adicionando a classe Sistema(Parte dos Atributos)

---
+++ src/br/edu/ufersa/LEVI/model/entity/Sistema.java
@@ -0,0 +1,12 @@
+package br.edu.ufersa.LEVI.model.entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Sistema {
+    private ArrayList<Cliente> clientes;
+    private ArrayList<Disco> discos;
+    private ArrayList<Livro> livros;
+    private ArrayList<Aluguel> alugueis;
+
+}
