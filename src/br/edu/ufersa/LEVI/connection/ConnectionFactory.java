44cad4acdec99a7a0e2b3938d4cfc14fcaa8bba3
Rafael José <110411739+Hypinha@users.noreply.github.com>
2026-05-30 14:30:20 -0300

    alterações na connection

--- src/br/edu/ufersa/LEVI/connection/ConnectionFactory.java
+++ src/br/edu/ufersa/LEVI/connection/ConnectionFactory.java
@@ -14,13 +14,30 @@ public class ConnectionFactory {
     // colocar a senha do banco de dados
     private static final String PASSWORD = "";
 
+    private static Connection con = null;
+
 
     public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+        // verifica se existe uma conexão em aberto antes de abrir uma nova
+        if ( con == null) {
+            try {
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
             // retorna o erro se o banco estiver desligado ou a senha de acesso esteja errada
             throw new RuntimeException("Erro ao conectar ao banco de dados: " + e.getMessage());
         }
+        }
+        return con;
     }
 }
+
+public static void closeConnection() {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                // retorna erro em caso de falha ao fechar uma comunicação
+                throw new RuntimeException("Erro ao fechar a conexão com o banco de dados!");
+            }
+        }
+    }
