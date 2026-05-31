755d65700f0f9fb35378ae94d9fa8e24d0bb3202
Joao-Neto75 <joaosilva.lgl@gmail.com>
2026-05-30 23:44:29 -0300

    Atualização da classe Livro

--- src/br/edu/ufersa/LEVI/model/entity/Livro.java
+++ src/br/edu/ufersa/LEVI/model/entity/Livro.java
@@ -1,145 +1,64 @@
 package br.edu.ufersa.LEVI.model.entity;
-
 import java.time.LocalDate;
 
-public class Livro {
 
-    private String titulo;
+public class Livro extends Produto {
     private String genero;
-    private LocalDate ano;
     private String autor;
-    private int exemplares;
     private int paginas;
-    private float valorAluguel;
 
     public Livro() {
-        this.titulo = "Sem título";
+        super();
         this.genero = "Sem gênero";
-        this.ano = LocalDate.now();
         this.autor = "Sem autor";
         this.paginas = 1;
-        this.exemplares = 0;
-        this.valorAluguel = 0;
     }
 
-
-    public Livro(String titulo, String genero, LocalDate ano, String autor, int paginas, int exemplares, float valorAluguel) {
-        setTitulo(titulo);
+    public Livro(String titulo, String genero, LocalDate ano, String autor,
+                 int paginas, int exemplares, float valorAluguel) {
+        super(titulo, exemplares, valorAluguel, ano);
         setGenero(genero);
-        setAno(ano);
         setAutor(autor);
         setPaginas(paginas);
-        setExemplares(exemplares);
-        setValorAluguel(valorAluguel);
     }
 
-
-    // Métodos
-    public void atualizarValorAluguel(float novoValor) {
-        setValorAluguel(novoValor);
+    @Override
+    public String getDescricao() {
+        return "Livro: " + getTitulo() + " | Autor: " + autor + " | Gênero: " + genero;
     }
 
-    public void adicionarExemplar(int quantidade) {
-        if (quantidade <= 0)
-            throw new IllegalArgumentException("Quantidade deve ser maior que zero!");
-        this.exemplares += quantidade;
+    @Override
+    public boolean contemTermo(String termo) {
+        return getTitulo().toLowerCase().contains(termo.toLowerCase())
+                || autor.toLowerCase().contains(termo.toLowerCase())
+                || genero.toLowerCase().contains(termo.toLowerCase());
     }
 
-    public void removerExemplar(int quantidade) {
-        if (quantidade <= 0)
-            throw new IllegalArgumentException("Quantidade deve ser maior que zero!");
-        if (quantidade > this.exemplares)
-            throw new IllegalArgumentException("Quantidade maior que o estoque disponível!");
-        this.exemplares -= quantidade;
-    }
-
-    public boolean verificarDisponibilidade() {
-        return this.exemplares > 0;
-    }
-
-
-    public float getValorAluguel() {
-        return valorAluguel;
-    }
-
-    public int getExemplares() {
-        return exemplares;
-    }
-
-    public int getPaginas() {
-        return paginas;
-    }
-
-    public LocalDate getAno() {
-        return ano;
+    // Getters e Setters específicos de Livro
+    public String getGenero() {
+        return genero;
     }
 
     public String getAutor() {
         return autor;
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    // Setters com validação
-    public void setTitulo(String titulo) {
-        if (titulo != null && !titulo.isEmpty()) {
-            this.titulo = titulo;
-        } else {
-            this.titulo = "Sem título";
-        }
+    public int getPaginas() {
+        return paginas;
     }
 
     public void setGenero(String genero) {
-        if (genero != null && !genero.isEmpty()) {
-            this.genero = genero;
-        } else {
-            this.genero = "Sem gênero";
-        }
-    }
-
-    public void setAno(LocalDate ano) {
-        if (ano != null && !ano.isAfter(LocalDate.now())) {
-            this.ano = ano;
-        } else {
-            this.ano = LocalDate.now();
-        }
+        if (genero != null && !genero.isEmpty()) this.genero = genero;
+        else this.genero = "Sem gênero";
     }
 
     public void setAutor(String autor) {
-        if (autor != null && !autor.isEmpty()) {
-            this.autor = autor;
-        } else {
-            this.autor = "Sem autor";
-        }
+        if (autor != null && !autor.isEmpty()) this.autor = autor;
+        else this.autor = "Sem autor";
     }
 
     public void setPaginas(int paginas) {
-        if (paginas > 0) {
-            this.paginas = paginas;
-        } else {
-            this.paginas = 1;
-        }
-    }
-
-    public void setExemplares(int exemplares) {
-        if (exemplares >= 0) {
-            this.exemplares = exemplares;
-        } else {
-            this.exemplares = 0;
-        }
-    }
-
-    public void setValorAluguel(float valorAluguel) {
-        if (valorAluguel >= 0) {
-            this.valorAluguel = valorAluguel;
-        } else {
-            this.valorAluguel = 0;
-        }
-    }
-
-    public String getGenero() {
-        return genero;
+        if (paginas > 0) this.paginas = paginas;
+        else this.paginas = 1;
     }
 }
