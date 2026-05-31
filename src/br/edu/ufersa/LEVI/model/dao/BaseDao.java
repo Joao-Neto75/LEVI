2b956b0da9430313b61c6750e9be9cca94d76286
Joao-Neto75 <joaosilva.lgl@gmail.com>
2026-05-31 11:06:49 -0300

    Atualizando baseDao. Tirando a duplicação da lógica da ConnectionFactory

--- src/br/edu/ufersa/LEVI/model/dao/BaseDao.java
+++ src/br/edu/ufersa/LEVI/model/dao/BaseDao.java
@@ -1,5 +1,6 @@
 package br.edu.ufersa.LEVI.model.dao;
 import java.sql.Connection;
+import br.edu.ufersa.LEVI.connection.ConnectionFactory;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.ResultSet;
@@ -7,37 +8,13 @@ import java.util.List;
 
 
 public interface BaseDao <E> {
-    // colocar o nome do banco de dados no final da url
-    final static String URL = "jdbc:mysql://localhost:3306/";
-
-    // colocar o user local do banco de dados
-    final static String USER = "";
-
-    // colocar a senha do banco de dados
-    final static String PASSWORD = "";
-
-    final static Connection con = getConnection();
-
 
     public static Connection getConnection() {
-            try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            // retorna o erro se o banco estiver desligado ou a senha de acesso esteja errada
-            throw new RuntimeException("Erro ao conectar ao banco de dados: " + e.getMessage());
-        }
+        return ConnectionFactory.getConnection();
     }
 
-
     public static void closeConnection() {
-        if (con != null) {
-            try {
-                con.close();
-            } catch (SQLException e) {
-                // retorna erro em caso de falha ao fechar uma comunicação
-                throw new RuntimeException("Erro ao fechar a conexão com o banco de dados!");
-            }
-        }
+        ConnectionFactory.closeConnection();
     }
 
 
