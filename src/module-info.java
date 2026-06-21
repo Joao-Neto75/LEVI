e24def3c690a1958a283e848100dc1cac61f9846
Joao-Neto75 <joaosilva.lgl@gmail.com>
2026-06-21 11:42:17 -0300

    Correção do Module-Info

--- src/module-info.java
+++ src/module-info.java
@@ -1,15 +1,13 @@
 module LEVI {
 
-
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
 
-    opens br.edu.ufersa.LEVI.view to javafx.fxml, javafx.graphics;
+    opens br.edu.ufersa.LEVI to javafx.fxml, javafx.graphics;
+    opens br.edu.ufersa.LEVI.view.Controller to javafx.fxml;
 
-
-
-    exports br.edu.ufersa.LEVI.view;
-
+    exports br.edu.ufersa.LEVI;
+    exports br.edu.ufersa.LEVI.view.Controller;
 
 }
