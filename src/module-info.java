f018733493e9b08d886a9dfb82d39ec094e4b4ae
yuriLeo-01 <yurileo2005@gmail.com>
2026-06-22 22:22:58 -0300

    feat: implementa TelaDiscos.fxml

--- src/module-info.java
+++ src/module-info.java
@@ -1,12 +1,9 @@
 module br.edu.ufersa.LEVI {
-
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
     opens br.edu.ufersa.LEVI to javafx.fxml, javafx.graphics;
     opens br.edu.ufersa.LEVI.view.Controller to javafx.fxml;
-
     exports br.edu.ufersa.LEVI;
     exports br.edu.ufersa.LEVI.view.Controller;
 }
