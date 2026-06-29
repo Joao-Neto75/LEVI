d99aa14c6fcbe9ffe5c3b50035ea98724a9b1b72
Yuri <yurileo2005@gmail.com>
2026-06-29 10:48:20 -0300

    fix: correções na classe Cliente.java

--- src/br/edu/ufersa/LEVI/model/entity/Cliente.java
+++ src/br/edu/ufersa/LEVI/model/entity/Cliente.java
@@ -1,8 +1,9 @@
 package br.edu.ufersa.LEVI.model.entity;
 
-public class Cliente {
+public class Cliente implements Pesquisavel{
 
     private String cpf;
+    private int id;
     private String nome;
     private String endereco;
 
@@ -26,27 +27,37 @@ public class Cliente {
         setNome(novoNome);
     }
 
+    @Override
+    public boolean contemTermo(String termo) {
+        return nome.toLowerCase().contains(termo.toLowerCase())
+                || cpf.contains(termo);
+    }
+
+
     // Getters
     public String getNome() { return nome; }
+    public int getId() { return id; }
     public String getEndereco() { return endereco; }
     public String getCpf() { return cpf; }
 
     // Setters com validação
     public void setNome(String nome) {
-        if (nome.isEmpty()){
-        this.nome = nome;}
+        if (!nome.isEmpty()){
+            this.nome = nome;}
         else {this.nome = "Fantasma";}
     }
 
+    public void setId(int id) { this.id = id; }
+
     public void setEndereco(String endereco) {
         if (!endereco.isEmpty()){
-        this.endereco = endereco;}
+            this.endereco = endereco;}
         else { this.endereco = "Sem endereço";}
     }
 
     public void setCpf(String cpf) {
         if (!cpf.isEmpty()){
-        this.cpf = cpf;}
+            this.cpf = cpf;}
         else {this.cpf = "Não existe";}
     }
 
